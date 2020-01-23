51bae583362c76a92a6c4e959aee7105242fe4df
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2020-01-23 23:32:56 +0530

    Added StoreHomePage.dart and modified main.dart

---
+++ coffee_store_app/lib/widget/StoreHomePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StoreHomePage extends StatelessWidget {
+
+  final String title;
+
+  const StoreHomePage({Key key, this.title}) : super(key: key);
+   
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
