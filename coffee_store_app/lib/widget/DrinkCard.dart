ff62e99a6f9aabe92ed40a6fec272610184e4feb
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2020-01-23 23:47:04 +0530

    Added new classes

---
+++ coffee_store_app/lib/widget/DrinkCard.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DrinkCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
