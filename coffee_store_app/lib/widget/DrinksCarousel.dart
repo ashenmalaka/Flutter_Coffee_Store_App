16b6ab483e0be1aa437b4ebfba735bfcc123cfd7
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2020-01-24 23:57:19 +0530

    Added a border to the drinks card widget

--- coffee_store_app/lib/widget/DrinksCarousel.dart
+++ coffee_store_app/lib/widget/DrinksCarousel.dart
@@ -7,8 +7,16 @@ class DrinksCarousel extends StatelessWidget {
     return SizedBox(
       height: 250.0,
       width: double.infinity,
-      child: DrinksCard(
-        drinkType: mainTypes[0],
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black,
+            width: 4.0, 
+            )
+          ),
+        child: DrinksCard(
+          drinkType: mainTypes[0],
+        ),
       ),
     );
   }
