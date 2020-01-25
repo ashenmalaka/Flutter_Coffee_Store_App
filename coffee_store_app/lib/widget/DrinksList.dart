4ca1adb8390a6483a38dae1dea4df7a291e618b8
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2020-01-25 23:24:27 +0530

    Added codes for DrinksList file

---
+++ coffee_store_app/lib/widget/DrinksList.dart
@@ -0,0 +1,18 @@
+import 'package:coffee_store_app/widget/DrinksCard.dart';
+import 'package:flutter/material.dart';
+
+class DrinksList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GridView.count(
+        crossAxisCount: 2,
+        children: coffeeTypes.map((drinkType){
+          return DrinksCard(
+            drinkType: drinkType,
+          )
+        }).toList(),
+      ),
+    );
+  }
+}
