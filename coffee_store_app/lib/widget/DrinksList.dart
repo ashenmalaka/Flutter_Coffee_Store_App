ef08373bdd9dc31f504dc0e34aaedba90ba0c763
ashenmalaka <37531139+ashenmalaka@users.noreply.github.com>
2020-02-04 23:04:12 +0530

    Added scoped model descendant to drinks list

--- coffee_store_app/lib/widget/DrinksList.dart
+++ coffee_store_app/lib/widget/DrinksList.dart
@@ -1,19 +1,24 @@
 import 'package:coffee_store_app/widget/DrinksCard.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class DrinksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: GridView.count(
-        crossAxisCount: 2,
-        padding: EdgeInsets.all(6.0),
-        children: coffeeTypes.map((drinkType){
+    return ScopedModelDescendant(
+      builder: (context, _, model){
+       return Expanded(
+        child: GridView.count(
+         crossAxisCount: 2,
+         padding: EdgeInsets.all(6.0),
+         children: coffeeTypes.map((drinkType){
           return DrinksCard(
             drinkType: drinkType,
-          );
-        }).toList(),
-      ),
-    );
+           );
+         }).toList(),
+       ),
+      );
+     }
+   );
   }
 }
